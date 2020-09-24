87f3e4aa5c688a70470bfa0cafc3bde0e82c7127
schwad22 <46706624+schwad22@users.noreply.github.com>
2020-09-24 17:38:39 +0100

    Final Submission

---
+++ src/unpackaged/abm/Animation/animationTest.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 12:03:02 2020
+
+"""
+
+import random
+import operator
+import matplotlib.pyplot
+import matplotlib.animation 
+
+num_of_agents = 10
+num_of_iterations = 100
+agents = []
+
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+
+
+#ax.set_autoscale_on(False)
+
+# Make the agents.
+for i in range(num_of_agents):
+    agents.append([random.randint(0,100),random.randint(0,100)])
+
+carry_on = True	
+	
+def update(frame_number):
+    
+    fig.clear()   
+    global carry_on
+    
+    for i in range(num_of_agents):
+        if random.random() < 0.5:
+            agents[i][0]  = (agents[i][0] + 1) % 99 
+        else:
+            agents[i][0]  = (agents[i][0] - 1) % 99
+        
+        if random.random() < 0.5:
+            agents[i][1]  = (agents[i][1] + 1) % 99 
+        else:
+            agents[i][1]  = (agents[i][1] - 1) % 99 
+        
+    if random.random() < 0.1:
+        carry_on = False
+        print("stopping condition")
+    
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i][0],agents[i][1])
+        print(agents[i][0],agents[i][1])
+
+		
+def gen_function(b = [0]):
+    a = 0
+    global carry_on #Not actually needed as we're not assigning, but clearer
+    while (a < 10) & (carry_on) :
+        yield a			# Returns control and waits next call.
+        a = a + 1
+
+
+#animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=10)
+animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False, )
+
+matplotlib.pyplot.show()
--- src/unpackaged/abm/Web_GUI/Readme.txt
+++ src/unpackaged/abm/Web_GUI/Readme.txt
@@ -1,5 +1,8 @@
 Agent Based Modelling Programme
 
+
+README
+
 A programme to animate the movement of 'n number of agents around a set environment whilst they interact with the environment and each other.
 
 The programme is run by calling model.py and passing in 3 arguments as text (number):
@@ -7,13 +10,30 @@ The programme is run by calling model.py and passing in 3 arguments as text (num
 	Number of agent moves (iterations of move)
 	Size of agents search neighbourhood
 
+No installation is necessary proving the user has Python3 already installed.
 The programme is run from a GUI that will initiate, and will animate their movement, the programme will run till all iterations have completed,
 or a stopping condition is met, when one agent has collected 100 pieces of the environment. 
 
-Issues
 
-Once complete the user must close the GUI and kill the programme by pressing crtl+c.
+LIBRARIES
+
+random
+matplotlib
+tkinter
+requests
+bs4
+csv
+sys
+
+
+ISSUES
+
+Once programme completes the user must close the GUI and kill the programme by pressing crtl+c.
 Documentation is only present for the agentsFramework class, as I couldn't get either Pydoc or Sphinx to write the documentation for model.py
 
 
+USAGE
+
+This programme is open source and permission is granted for wider use.
+Licence - Apache License 2.0
 
